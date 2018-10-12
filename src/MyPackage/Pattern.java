8cc44d72dbb96ead6d9d510cd231adf28f90b8ea
Nisho <nisho.p@thirdware.com>
2018-10-12 14:13:11 +0530

    Pattern updated

--- src/MyPackage/Pattern.java
+++ src/MyPackage/Pattern.java
@@ -1,17 +1,23 @@
 package MyPackage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Pattern {
-	public static void main(String s[]) {
-		String word="JAVAJ2EE";
+	public static void main(String s[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter the word: ");
+		String word = br.readLine();
 		for (int i = 0; i < word.length(); i++) {
-			for (int j = 0; j < i; j++) {
-				System.out.print(word.charAt(j));
+			for (int j = 0; j <= i; j++) {
+				System.out.print(word.charAt(j) + " ");
 			}
 			System.out.println();
 		}
-		for (int i = word.length(); i >= 0; i--) {
+		for (int i = word.length() - 1; i >= 0; i--) {
 			for (int j = 0; j < i; j++) {
-				System.out.print(word.charAt(j));
+				System.out.print(word.charAt(j) + " ");
 			}
 			System.out.println();
 		}
